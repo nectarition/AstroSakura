01da39efe600ef3aca22b27638529a079800926e
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-03-23 18:07:44 +0900

    config

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+const config = {
+  siteUrl: 'https://kohatabe.jp',
+  copyright: {
+    year: 2021,
+    author: 'Nectarition'
+  },
+  event: {
+    name: 'AstroSakura',
+    date: '2023-06-18',
+    place: '開催場所',
+    keyVisualAuthor: 'キービジュアル',
+    content: 'イベント内容',
+    twitterId: 'nct_kohatabe'
+  }
+}
+
+export default config
