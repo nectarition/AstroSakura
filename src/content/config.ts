191bec4c160a87dcbde964c37a5fece65f6ecff8
SAIGUSA Tomotada <smmy@nectarition.jp>
2024-10-28 21:52:26 +0900

    アーカイブ対応

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,7 +11,8 @@ const pageCollection = defineCollection({
         name: z.string(),
         path: z.string()
       }))
-      .optional()
+      .optional(),
+    isArchive: z.boolean().optional(),
   })
 })
 
